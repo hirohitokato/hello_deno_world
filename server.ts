ea95f29d5606fe2c78451a60859f949c0a933187
Hirohito Kato <hkato193@gmail.com>
2024-12-21 15:55:25 +0900

    adds a cron job of every 1 minute.

--- server.ts
+++ server.ts
@@ -1,9 +1,14 @@
 const kv = await Deno.openKv();
 
-Deno.serve(async (_req) => {
-    await kv.atomic().sum(["visitors"], 1n).commit();
-    
-    const count = await kv.get(["visitors"]);
+// Run a task every 1 minute.
+Deno.cron("Increment a counter", "* * * * *", async () => {
+  await kv.atomic().sum(["visitors"], 1n).commit();
+});
 
-    return new Response(`Hello, #${count.value} World!`);
+Deno.serve(async (_req) => {
+  await kv.atomic().sum(["visitors"], 1n).commit();
+
+  const count = await kv.get(["visitors"]);
+
+  return new Response(`Hello, #${count.value} World!`);
 });
