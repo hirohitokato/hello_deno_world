082575683928ae277dff217336b6fec07ed9c50a
Hirohito Kato <hkato193@gmail.com>
2024-12-21 15:47:13 +0900

    Add key value storage access

--- server.ts
+++ server.ts
@@ -1,3 +1,9 @@
-Deno.serve((_req) => {
-    return new Response("Hello, World!");
+const kv = await Deno.openKv();
+
+Deno.serve(async (_req) => {
+    await kv.atomic().sum(["visitors"], 1n).commit();
+    
+    const count = await kv.get(["visitors"]);
+
+    return new Response(`Hello, #${count.value} World!`);
 });
